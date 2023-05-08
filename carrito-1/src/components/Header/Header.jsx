bbe085cf9fa5d0db3e17e0aa93a44e144cd9c110
Ezequiel Villalba <60276551+therzer@users.noreply.github.com>
2023-05-08 16:53:23 -0300

    4

---
+++ carrito-1/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+            <h1><box-icon type='solid' name='shower'></box-icon>Header</h1>
+        </div>
+    )
+}
